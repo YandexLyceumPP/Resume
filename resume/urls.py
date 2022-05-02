6efedb31bd460e8fc3e348ff715faa01aaaa1e74
GRusl <ffhshdxva@gmail.com>
2022-05-02 13:11:03 +0300

    Initial url markup for the "about" application

--- resume/urls.py
+++ resume/urls.py
@@ -4,6 +4,7 @@ from django.urls import include, path
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('about/', include('about.urls', namespace='about')),
     path('admin/', admin.site.urls),
 ]
 
