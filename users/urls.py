1ea7a30b14f28556c720ff6a575b2041add91e9f
GRusl <ffhshdxva@gmail.com>
2022-05-13 03:40:09 +0300

    A blank for the resume card has been created

--- users/urls.py
+++ users/urls.py
@@ -1,28 +1,30 @@
 from django.urls import path
 
-
 from users.views import *
 
 app_name = "users"
 
 urlpatterns = [
     path("", profile, name="profile"),
-    path("detail/<str:user_name>/", user_detail, name="detail"),
+
     path("signup/", signup, name="signup"),
     path("login/", LoginView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
+
     path("password_change/", PasswordChangeView.as_view(), name="password_change"),
     path(
         "password_change/done/",
         PasswordChangeDoneView.as_view(),
         name="password_change_done",
     ),
+
     path("password_reset/", PasswordResetView.as_view(), name="password_reset"),
     path(
         "password_reset/done/",
         PasswordResetDoneView.as_view(),
         name="password_reset_done",
     ),
+
     path(
         "reset/<uidb64>/<token>/",
         PasswordResetConfirmView.as_view(),
@@ -33,4 +35,6 @@ urlpatterns = [
         PasswordResetCompleteView.as_view(),
         name="password_reset_complete",
     ),
+
+    path("<str:user_name>/", user_detail, name="detail"),
 ]
