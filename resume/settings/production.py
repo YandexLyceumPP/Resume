6a1ea9afc640f86cbd95acfc17b130f6db046d92
lav <me@lavn.ml>
2022-05-17 21:13:30 +0300

    add email backend (files for debug & smtp for prod)

--- resume/settings/production.py
+++ resume/settings/production.py
@@ -13,3 +13,11 @@ DATABASES = {
 }
 
 DEBUG = False
+
+EMAIL_USE_SSL = config("EMAIL_USE_SSL", default=True, cast=bool)
+EMAIL_HOST = config("EMAIL_HOST", default="smtp.yandex.ru")
+EMAIL_PORT = config("EMAIL_PORT", default=465, cast=int)
+EMAIL_HOST_USER = config("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
