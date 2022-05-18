ffbc4349e48fc0db2fd839c03b2faa7a87bde2b6
lav <me@lavn.ml>
2022-05-18 21:27:04 +0300

    add resume form and template update

--- workshop/views.py
+++ workshop/views.py
@@ -1,8 +1,19 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from workshop.forms import CreateResumeForm
 
 
 @login_required
 def workshop(request):
-    TEMPLATE = "workshop/create.html"
-    return render(request, TEMPLATE)
+    if request.method == "POST":
+        form = CreateResumeForm(request.POST or None)
+        if form.is_valid():
+            form.save()
+            return redirect("users:profile")
+    else:
+        form = CreateResumeForm(request.POST or None)
+    context = {
+        "form": form,
+    }
+    return render(request, "workshop/create.html", context=context)
