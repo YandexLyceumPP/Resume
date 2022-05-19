8e2a2634ab6d00727eb6e04feebeee888abb701f
GRusl <ffhshdxva@gmail.com>
2022-05-19 17:20:43 +0300

    Decorated with a skill selection form

--- users/forms.py
+++ users/forms.py
@@ -35,7 +35,7 @@ class UserForm(forms.ModelForm, BaseForm):
 class AddSkillForm(forms.ModelForm):
     skills = forms.ModelMultipleChoiceField(
         queryset=Skill.objects.all().only("skill"),
-        widget=forms.CheckboxSelectMultiple
+        widget=forms.CheckboxSelectMultiple(attrs={"class": "form-check-input me-1"})
     )
 
     class Meta:
