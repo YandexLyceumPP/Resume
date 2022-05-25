0bdf91a6b9b952785dbb59ef31e7637c44a5673c
6ocra3 <6o3kmm@mail.ru>
2022-05-25 12:07:31 +0300

    make 404 error html

--- homepage/views.py
+++ homepage/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 def home(request):
     TEMPLATE = "homepage/homepage.html"
     return render(request, TEMPLATE)
+
+def error_404(request, exception):
+    TEMPLATE = "errors/error_404.html"
+    return render(request, TEMPLATE, status=404)
